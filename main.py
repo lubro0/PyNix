ffab5bc6ca29509197575556c2304de43491721e
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-12-27 16:13:35 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-user_input = input("pl: ")
-if user_input == "pl":
-    import os
-    os.system("mkdir plugins")
-    os.system("cd plugins")
-    with open("plugins/README.txt", "w") as file:
-        file.write("SOON")
-    os.system("cd ~")
-  
